4b983dc8760b79b7ac4c276f083963d0ff698d81
Taketo Sano <taketo1024@gmail.com>
2019-02-27 13:40:59 +0900

    impl PlaneView, VectorView.

--- LinAlg/Views/PlaneView.swift
+++ LinAlg/Views/PlaneView.swift
@@ -8,16 +8,8 @@
 
 import UIKit
 
-extension CGPoint {
-    init(_ x: CGFloat, _ y: CGFloat) {
-        self.init(x: x, y: y)
-    }
-}
-
 class PlaneView: UIView {
-    var pointSize: CGFloat = 8.0
-    var unit: CGFloat = 50.0
-    var scale: CGFloat = 1.0
+    var unitLength: CGFloat = 50.0
     
     var width: CGFloat {
         return bounds.width
@@ -27,10 +19,28 @@ class PlaneView: UIView {
         return bounds.height
     }
     
+    var planeOrigin: CGPoint {
+        return CGPoint(width / 2, height / 2)
+    }
+    
+    var vectorViews: [VectorView] {
+        return subviews.compactMap{ $0 as? VectorView }
+    }
+    
+    func convert(_ v: Vec2) -> CGPoint {
+        let p = v.asCGPoint
+        return CGPoint(
+            width  / 2 + p.x * unitLength,
+            height / 2 - p.y * unitLength
+        )
+    }
+    
     override func draw(_ rect: CGRect) {
         guard let ctx = UIGraphicsGetCurrentContext() else {
             return
         }
+        
+        let center = bounds.center
         
         // fill background
         ctx.setFillColor(UIColor.white.cgColor)
@@ -39,12 +49,40 @@ class PlaneView: UIView {
         // draw axises
         ctx.setLineWidth(1)
         ctx.setStrokeColor(UIColor.black.cgColor)
-        ctx.move(to: CGPoint(0, height / 2))
-        ctx.addLine(to: CGPoint(width, height / 2))
+        ctx.move(to: CGPoint(0, center.y))
+        ctx.addLine(to: CGPoint(width, center.y))
         ctx.strokePath()
         
-        ctx.move(to: CGPoint(width / 2, 0))
-        ctx.addLine(to: CGPoint(width / 2, height))
+        ctx.move(to: CGPoint(center.x, 0))
+        ctx.addLine(to: CGPoint(center.x, height))
         ctx.strokePath()
+        
+        // vector arrows
+        for v in vectorViews {
+            if v.isHidden { continue }
+            ctx.setLineWidth(v.lineWidth)
+            ctx.setStrokeColor(v.color.cgColor)
+            ctx.move(to: convert(.zero))
+            ctx.addLine(to: convert(v.vector))
+            ctx.strokePath()
+        }
+    }
+    
+    func add(_ v: Vec2, color: UIColor = .black) {
+        let vecView = VectorView(frame: CGRect(0, 0, 34, 34))
+        vecView.vector = v
+        vecView.color = color
+        vecView.backgroundColor = .clear
+        addSubview(vecView)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        setNeedsDisplay()
+        
+        for vecView in vectorViews {
+            vecView.frame.origin = convert(vecView.vector) - vecView.bounds.center
+            vecView.setNeedsDisplay()
+        }
     }
 }
--- LinAlg/Views/VectorView.swift
+++ LinAlg/Views/VectorView.swift
@@ -10,32 +10,38 @@ import UIKit
 
 class VectorView: UIView {
     
-    var vector: CGPoint = .zero
-    var color: UIColor = .red
-    var width: CGFloat = 2
+    var vector: Vec2 = .zero
     
-//    var plane: PlaneView {
-//        return superview! as! PlaneView
-//    }
-//    
-//    override func didMoveToSuperview() {
-//        if superview is PlaneView {
-//            fatalError()
-//        }
-//    }
+    var color: UIColor = .black
+    var lineWidth: CGFloat = 2
+    
+    var headRadius: CGFloat {
+        return lineWidth * 2.5
+    }
     
     override func draw(_ rect: CGRect) {
-        self.clipsToBounds = false
-        
         guard let ctx = UIGraphicsGetCurrentContext() else {
             return
         }
         
-        // draw axises
-        ctx.setLineWidth(width)
-        ctx.setStrokeColor(color.cgColor)
-        ctx.move(to: .zero)
-        ctx.addLine(to: vector)
-        ctx.strokePath()
+        let c = bounds.center
+        let r = headRadius
+        
+        ctx.setFillColor(color.cgColor)
+        
+        if vector == .zero {
+            ctx.fillEllipse(in: CGRect(c.x - r, c.y - r, 2 * r, 2 * r))
+        } else {
+            let π = CGFloat.pi
+            let t = -vector.asCGPoint.arg
+            let u = CGPoint.unit
+            
+            ctx.beginPath()
+            ctx.move(to: c + r * u(t))
+            ctx.addLine(to: c + r * u(t + 2 * π / 3))
+            ctx.addLine(to: c + r * u(t + 4 * π / 3))
+            ctx.closePath()
+            ctx.fillPath()
+        }
     }
 }
